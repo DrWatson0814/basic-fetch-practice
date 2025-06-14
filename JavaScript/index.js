9d86ca40d0153a32fc8c2180080194ba58cd7539
unknown <shirleyhaddix82@outlook.com>
2025-06-13 20:00:25 -0400

    Created file structure. Added HTML structure, Began CSS

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -8,21 +8,32 @@
 //   }
 // }
 
+// function displayUnder10(dataToProcess) {
+//   let usersUnder10 = [];
+//     if (dataToProcess < 10) {
+//     usersUnder10.unshift(dataToProcess);
+//     console.log(usersUnder10);
+//   }
+// }
+
+
+function displayUnder10(employees) {
+
+}
 
 
 async function under10() {
-    try {
-        const response = await fetch("https://dan-collins-dev.github.io/dummy-data-fetching-repo/data/users.json");
-        const data = await response.json()
-        if(data.yearsEmployed < 10) {
-            console.log(data);
+  try {
+    const response = await fetch("https://dan-collins-dev.github.io/dummy-data-fetching-repo/data/users.json");
+    const data = await response.json();
+    for (const elements of data) {
+        if(elements.yearsEmployed < 10) {
+            console.log(elements);
         }
     }
-    catch(error) {
-        console.error(error);
+    } catch (error) {
+    console.error(error);
     }
-}
-// const btn = document.getElementById("get-all-btn");
-// btn.addEventListener("click", getUsers(btn));
+};
 
 under10();
