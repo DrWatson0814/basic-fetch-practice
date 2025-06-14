2c305f4768b7b84cf9577139803098ffd6cc0a88
unknown <shirleyhaddix82@outlook.com>
2025-06-14 02:48:22 -0400

    Wrote get-all-users & under-10-user. On creating cards

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,24 +1,11 @@
-// async function getUsers() {
-//   try {
-//     const get = await fetch("https://dan-collins-dev.github.io/dummy-data-fetching-repo/data/users.json");
-//     const response = await get.json();
-//     console.log(response);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-
-// function displayUnder10(dataToProcess) {
-//   let usersUnder10 = [];
-//     if (dataToProcess < 10) {
-//     usersUnder10.unshift(dataToProcess);
-//     console.log(usersUnder10);
-//   }
-// }
-
-
-function displayUnder10(employees) {
-
+async function getUsers() {
+  try {
+    const get = await fetch("https://dan-collins-dev.github.io/dummy-data-fetching-repo/data/users.json");
+    const response = await get.json();
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 
@@ -27,13 +14,57 @@ async function under10() {
     const response = await fetch("https://dan-collins-dev.github.io/dummy-data-fetching-repo/data/users.json");
     const data = await response.json();
     for (const elements of data) {
-        if(elements.yearsEmployed < 10) {
-            console.log(elements);
-        }
+      if (elements.yearsEmployed < 10) {
+        console.log(elements);
+      }
     }
-    } catch (error) {
+    }
+    catch(error) {
     console.error(error);
     }
+}
+
+function cardReset() {
+  const resetCards = document.getElementsByClassName("cardElement");
+  resetCards.remove();
+}
+
+const getAll = document.getElementById("get-all-btn").onclick = getUsers;
+const reset = document.getElementById('reset-btn').onclick = cardReset;
+const below10 = document.getElementById('users-10-btn').onclick = under10;
+
+async function allTogether(btn) {
+    const cardData = await btn;
+    createCard(cardData);
 };
 
-under10();
+
+function createCard(obj) {
+  const cardDiv = document.getElementById("cards");
+  for (const element of obj) {
+    const card = document.createElement("article")
+    const name = document.createElement("h2")
+    card.classList.add("cardElement");
+    const email = document.createElement("p")
+    card.classList.add("cardElement");
+    const compName = document.createElement("p")
+    card.classList.add("cardElement");
+    const yearsEmp = document.createElement("p")
+    card.classList.add("cardElement");
+
+    name.textContent = `${element.firstName} ${element.lastName}`;
+    email.textContent = `${element.email}`;
+    compName.textContent = `${element.companyName}`;
+    yearsEmp.textContent = `${element.yearsEmployed}`;
+
+    card.appendChild(name);
+    card.appendChild(email);
+    card.appendChild(compName);
+    card.appendChild(yearsEmp);
+
+    cardDiv.appendChild(card);
+  }
+}
+
+
+
