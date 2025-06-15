4db4e16c3c0ab92de33b254ee7dbc7d06c18b4d3
unknown <shirleyhaddix82@outlook.com>
2025-06-14 22:16:10 -0400

    Finished coding functions and button. On creating media query

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,65 +1,65 @@
-const getAll = document.getElementById("get-all-btn").onclick = getUsers;
-const reset = document.getElementById('reset-btn').onclick = cardReset;
-const below10 = document.getElementById('users-10-btn').onclick = under10;
+const getAll = document.getElementById("get-all-btn").addEventListener('click', getUsers);
+const reset = document.getElementById('reset-btn').addEventListener('click', cardReset);
+const below10 = document.getElementById('users-10-btn').addEventListener('click', under10);
 
 
 
+
+
+// Get All Employees Function
 async function getUsers() {
   try {
     const get = await fetch("https://dan-collins-dev.github.io/dummy-data-fetching-repo/data/users.json");
     const response = await get.json();
-    console.log(response);
-  } catch (error) {
+    createCard(response);
+    } catch (error) {
     console.error(error);
   }
 }
 
 
+
+
+// Get Users Under 10 Years 
 async function under10() {
   try {
     const response = await fetch("https://dan-collins-dev.github.io/dummy-data-fetching-repo/data/users.json");
     const data = await response.json();
-    for (const elements of data) {
-      if (elements.yearsEmployed < 10) {
-        console.log(elements);
-      }
-    }
+    const empUnder10 = data.filter(user => user.yearsEmployed < 10);
+    createCard(empUnder10);
   }  
     catch(error) {
     console.error(error);
   }
 }
 
+
+
+// Reset Function
 function cardReset() {
-  const resetCards = document.getElementsByClassName("cardElement");
-  resetCards.remove();
+  const resetCards = document.getElementById("cards");
+  resetCards.innerHTML = '';
 }
 
 
 
-async function allTogether(btn) {
-    const cardData = await btn;
-    createCard(cardData);
-};
 
-
+// Card Creatsion Function
 function createCard(obj) {
   const cardDiv = document.getElementById("cards");
+  cardDiv.innerHTML = '';
   for (const element of obj) {
     const card = document.createElement("article")
+    card.classList.add("cardElement");
     const name = document.createElement("h2")
-    card.classList.add("cardElement");
     const email = document.createElement("p")
-    card.classList.add("cardElement");
     const compName = document.createElement("p")
-    card.classList.add("cardElement");
     const yearsEmp = document.createElement("p")
-    card.classList.add("cardElement");
 
-    name.textContent = `${element.firstName} ${element.lastName}`;
-    email.textContent = `${element.email}`;
-    compName.textContent = `${element.companyName}`;
-    yearsEmp.textContent = `${element.yearsEmployed}`;
+    name.textContent = `Name: ${element.firstName} ${element.lastName}`;
+    email.textContent = `Email: ${element.email}`;
+    compName.textContent = `Company Name: ${element.companyName}`;
+    yearsEmp.textContent = `Years of Service: ${element.yearsEmployed}`;
 
     card.appendChild(name);
     card.appendChild(email);
